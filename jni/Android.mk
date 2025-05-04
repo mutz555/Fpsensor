fd7bf1237cf155c38f209112a329213d948ea7b1
mutz555 <mrbosscoc22@gmail.com>
2025-05-04 09:47:52 +0700

    Update Android.mk

--- jni/Android.mk
+++ jni/Android.mk
@@ -1,13 +1,21 @@
 LOCAL_PATH := $(call my-dir)
 
+# Build our module without external dependencies
 include $(CLEAR_VARS)
-LOCAL_MODULE := module
-LOCAL_SRC_FILES := zygisk_main.cpp
+LOCAL_MODULE := fingerprint_bypasser
+# Make sure we only use main.cpp and no other file
+LOCAL_SRC_FILES := main.cpp
 LOCAL_STATIC_LIBRARIES := libcxx
-LOCAL_LDLIBS := -llog
+LOCAL_LDLIBS := -llog -ldl
+LOCAL_CFLAGS += -std=c++17 -fno-rtti -fno-exceptions -DDISABLE_DOBBY_HOOK
+
+# Define that we're not using Dobby
+LOCAL_CPPFLAGS += -DNO_DOBBY_LIBRARY
+
+# Explicitly disable all external libraries (including Dobby)
+LOCAL_SHARED_LIBRARIES :=
+
 include $(BUILD_SHARED_LIBRARY)
 
-include $(CLEAR_VARS)
-LOCAL_MODULE := libcxx
-LOCAL_SRC_FILES := 
-include $(BUILD_STATIC_LIBRARY)
+# Only import libcxx, no other external dependencies
+$(call import-module,libcxx)
