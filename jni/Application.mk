3b752423197f7650b52b0ead54b9c068b5011696
mutz555 <mrbosscoc22@gmail.com>
2025-05-04 09:20:09 +0700

    Update Application.mk

--- jni/Application.mk
+++ jni/Application.mk
@@ -1,4 +1,4 @@
 APP_ABI := armeabi-v7a arm64-v8a x86 x86_64
-APP_PLATFORM := android-34
+APP_PLATFORM := android-33
 APP_STL := c++_static
 APP_CFLAGS := -std=c++17 -Wall -fpermissive -fexceptions -frtti -DANDROID_R_API=30 -DANDROID_S_API=31 -DANDROID_T_API=33 -DANDROID_U_API=34 -DANDROID_V_API=35
