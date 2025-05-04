31accf6cec0aa970b3e80bc523ec4a9efdd899b2
mutz555 <mrbosscoc22@gmail.com>
2025-05-05 01:25:52 +0700

    Update Android.mk

--- jni/Android.mk
+++ jni/Android.mk
@@ -1,12 +1,12 @@
 LOCAL_PATH := $(call my-dir)
 
-# Modul static library untuk xhook (file C)
+# ===== Modul Static Library untuk xhook (file C saja) =====
 include $(CLEAR_VARS)
 LOCAL_MODULE := xhook
 LOCAL_SRC_FILES := xhook.c
 include $(BUILD_STATIC_LIBRARY)
 
-# Modul utama fingerprint_bypasser (file C++)
+# ===== Modul Shared Library utama (file C++ saja) =====
 include $(CLEAR_VARS)
 LOCAL_MODULE := fingerprint_bypasser
 LOCAL_SRC_FILES := main.cpp
@@ -14,5 +14,4 @@ LOCAL_STATIC_LIBRARIES := xhook
 LOCAL_LDLIBS := -llog -ldl
 LOCAL_CFLAGS += -fno-rtti -fno-exceptions -DDISABLE_DOBBY_HOOK
 LOCAL_CPPFLAGS += -std=c++17 -DNO_DOBBY_LIBRARY
-LOCAL_SHARED_LIBRARIES :=
 include $(BUILD_SHARED_LIBRARY)
