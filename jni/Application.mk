bf5120c241f911f9cc96956e795bb02c80906461
mutz555 <mrbosscoc22@gmail.com>
2025-05-05 01:41:02 +0700

    Update Application.mk

--- jni/Application.mk
+++ jni/Application.mk
@@ -1,4 +1,6 @@
 APP_ABI := armeabi-v7a arm64-v8a x86 x86_64
 APP_PLATFORM := android-33
 APP_STL := c++_static
-APP_CFLAGS := -std=c++17 -Wall -fpermissive -fexceptions -frtti -DANDROID_R_API=30 -DANDROID_S_API=31 -DANDROID_T_API=33 -DANDROID_U_API=34 -DANDROID_V_API=35
+# Hilangkan -std=c++17 dari APP_CFLAGS!
+APP_CFLAGS := -Wall -fpermissive -fexceptions -frtti -DANDROID_R_API=30 -DANDROID_S_API=31 -DANDROID_T_API=33 -DANDROID_U_API=34 -DANDROID_V_API=35
+# Jika butuh -std=c++17, tambahkan di Android.mk bagian LOCAL_CPPFLAGS saja
