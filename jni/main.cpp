9cbd7ae48d5769809b0cdfadba9857a7f5db9a5b
mutz555 <mrbosscoc22@gmail.com>
2025-05-07 22:38:41 +0700

    Update main.cpp

--- jni/main.cpp
+++ jni/main.cpp
@@ -11,7 +11,7 @@
 #include "xhook.h"
 
 // For Zygisk
-#include "zygisk.h"
+#include "zygisk.hpp"
 
 #define LOG_TAG "XHookJNI"
 #define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
