741ecfab15be135a6a6f91281a86fe896b6a1bd3
mutz555 <mrbosscoc22@gmail.com>
2025-05-09 03:56:32 +0700

    Update main.cpp

--- jni/main.cpp
+++ jni/main.cpp
@@ -1,34 +1,62 @@
-#include <jni.h> #include <unistd.h> #include <android/log.h> #include <dlfcn.h> #include <cstring> #include <cstdlib> #include <sys/system_properties.h> #include "xhook.h" #include "zygisk.hpp"
+#include <jni.h>
+#include <cstring>
+#include <unistd.h>
+#include <sys/system_properties.h>
+#include <android/log.h>
+#include "zygisk.hpp"
 
-#define LOG_TAG "ZygiskSpoof" #define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, VA_ARGS)
+#define LOG_TAG "ZygiskSpoof"
+#define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
 
-const char *target_apps[] = { "com.mobile.legends", "com.miHoYo.GenshinImpact", "com.miHoYo.hkrpg", "com.tencent.ig", "com.activision.callofduty.shooter" };
+// List package target
+const char *target_apps[] = {
+    "com.mobile.legends",
+    "com.miHoYo.GenshinImpact",
+    "com.miHoYo.honkaiimpact3",
+    "com.tencent.ig",
+    "com.activision.callofduty.shooter",
+    nullptr
+};
 
-bool shouldSpoof(const char *packageName) { for (const char *target : target_apps) { if (strcmp(packageName, target) == 0) return true; } return false; }
-
-void spoof_props() { struct prop_pair { const char *key; const char *val; } props[] = { {"ro.product.brand", "samsung"}, {"ro.product.manufacturer", "samsung"}, {"ro.product.device", "dm3q"}, {"ro.product.model", "SM-S928B"}, {"ro.product.name", "dm3qxx"}, {"ro.build.product", "dm3q"}, {"ro.build.fingerprint", "samsung/dm3qxx/dm3q:14/UP1A.231005.007/S928BXXU1AXB5:user/release-keys"}, {"ro.build.version.release", "14"}, {"ro.build.version.sdk", "34"}, {"ro.board.platform", "kalama"}, {"ro.hardware.soc.manufacturer", "Qualcomm Technologies, Inc."}, {"ro.hardware.soc.model", "SM8650"} };
-
-for (auto &p : props) {
-    __system_property_set(p.key, p.val);
-    LOGI("Spoofed %s = %s", p.key, p.val);
-}
-
-}
-
-class SpoofModule : public zygisk::ModuleBase { public: void onLoad(zygisk::Api *api, JNIEnv *env) override { LOGI("[ZygiskSpoof] Module loaded"); }
-
-void preAppSpecialize(zygisk::AppSpecializeArgs *args) override {
-    const char *pkg = args->nice_name ? args->env->GetStringUTFChars(args->nice_name, nullptr) : "";
-    if (shouldSpoof(pkg)) {
-        LOGI("[ZygiskSpoof] Target app detected: %s", pkg);
-        spoof_props();
-    } else {
-        LOGI("[ZygiskSpoof] Non-target app: %s", pkg);
+bool shouldSpoof(const char *packageName) {
+    for (const char **target = target_apps; *target; ++target) {
+        if (strcmp(packageName, *target) == 0) return true;
     }
-    if (pkg && args->nice_name) args->env->ReleaseStringUTFChars(args->nice_name, pkg);
+    return false;
 }
 
+void set_prop(const char *key, const char *val) {
+    if (__system_property_set(key, val) == 0) {
+        LOGI("Spoofed %s = %s", key, val);
+    }
+}
+
+class SpoofModule : public zygisk::ModuleBase {
+public:
+    void onLoad(zygisk::Api *api, JNIEnv *env) override {
+        LOGI("ZygiskSpoof Module loaded");
+    }
+
+    void preAppSpecialize(zygisk::AppSpecializeArgs *args) override {
+        const char *pkg = args->packageName;
+        if (!pkg) return;
+
+        if (shouldSpoof(pkg)) {
+            LOGI("Target app detected: %s", pkg);
+
+            set_prop("ro.product.brand", "samsung");
+            set_prop("ro.product.manufacturer", "samsung");
+            set_prop("ro.product.model", "SM-S928B");
+            set_prop("ro.product.device", "dm3q");
+            set_prop("ro.build.fingerprint", "samsung/dm3qxx/dm3q:14/UP1A.231005.007/S928BXXU1AXB5:user/release-keys");
+            set_prop("ro.board.platform", "kalama");
+            set_prop("ro.hardware", "qcom");
+            set_prop("ro.soc.manufacturer", "Qualcomm Technologies, Inc.");
+            set_prop("ro.soc.model", "SM8650");
+        } else {
+            LOGI("Non-target app: %s", pkg);
+        }
+    }
 };
 
 REGISTER_ZYGISK_MODULE(SpoofModule)
-
