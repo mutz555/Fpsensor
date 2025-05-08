125906d2e612e8ebe8c1daebee8649261300149e
mutz555 <mrbosscoc22@gmail.com>
2025-05-09 04:14:38 +0700

    Update main.cpp

--- jni/main.cpp
+++ jni/main.cpp
@@ -1,20 +1,16 @@
-#include <jni.h>
 #include <cstring>
-#include <unistd.h>
-#include <sys/system_properties.h>
 #include <android/log.h>
 #include "zygisk.hpp"
+#include <sys/system_properties.h>
 
-#define LOG_TAG "ZygiskSpoof"
-#define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
+#define LOGI(...) __android_log_print(ANDROID_LOG_INFO, "[ZygiskSpoof]", __VA_ARGS__)
 
-// List package target
-const char *target_apps[] = {
+static const char *target_apps[] = {
     "com.mobile.legends",
     "com.miHoYo.GenshinImpact",
-    "com.miHoYo.honkaiimpact3",
+    "com.miHoYo.hkrpg",
     "com.tencent.ig",
-    "com.activision.callofduty.shooter",
+    "com.garena.game.codm",
     nullptr
 };
 
@@ -25,38 +21,41 @@ bool shouldSpoof(const char *packageName) {
     return false;
 }
 
-void set_prop(const char *key, const char *val) {
-    if (__system_property_set(key, val) == 0) {
-        LOGI("Spoofed %s = %s", key, val);
-    }
-}
-
 class SpoofModule : public zygisk::ModuleBase {
+private:
+    zygisk::Api *zygisk_api = nullptr;
+    JNIEnv *zygisk_env = nullptr;
+
 public:
     void onLoad(zygisk::Api *api, JNIEnv *env) override {
-        LOGI("ZygiskSpoof Module loaded");
+        zygisk_api = api;
+        zygisk_env = env;
+        LOGI("ZygiskSpoof module loaded");
     }
 
-    void preAppSpecialize(zygisk::AppSpecializeArgs *args) override {
-        const char *pkg = args->packageName;
-        if (!pkg) return;
-
+    void postAppSpecialize(const zygisk::AppSpecializeArgs *args) override {
+        const char *pkg = zygisk_env->GetStringUTFChars(args->nice_name, nullptr);
         if (shouldSpoof(pkg)) {
             LOGI("Target app detected: %s", pkg);
-
-            set_prop("ro.product.brand", "samsung");
-            set_prop("ro.product.manufacturer", "samsung");
-            set_prop("ro.product.model", "SM-S928B");
-            set_prop("ro.product.device", "dm3q");
-            set_prop("ro.build.fingerprint", "samsung/dm3qxx/dm3q:14/UP1A.231005.007/S928BXXU1AXB5:user/release-keys");
-            set_prop("ro.board.platform", "kalama");
-            set_prop("ro.hardware", "qcom");
-            set_prop("ro.soc.manufacturer", "Qualcomm Technologies, Inc.");
-            set_prop("ro.soc.model", "SM8650");
+            __system_property_set("ro.product.model", "SM-S928B");
+            __system_property_set("ro.product.brand", "samsung");
+            __system_property_set("ro.product.manufacturer", "samsung");
+            __system_property_set("ro.product.device", "dm3q");
+            __system_property_set("ro.product.name", "dm3qxx");
+            __system_property_set("ro.build.display.id", "UP1A.231005.007");
+            __system_property_set("ro.build.id", "UP1A.231005.007");
+            __system_property_set("ro.build.tags", "release-keys");
+            __system_property_set("ro.build.type", "user");
+            __system_property_set("ro.build.user", "dpi");
+            __system_property_set("ro.build.host", "21DJB");
+            __system_property_set("ro.board.platform", "kalama");
+            __system_property_set("ro.soc.manufacturer", "Qualcomm Technologies, Inc.");
+            __system_property_set("ro.soc.model", "SM8650");
         } else {
             LOGI("Non-target app: %s", pkg);
         }
+        zygisk_env->ReleaseStringUTFChars(args->nice_name, pkg);
     }
 };
 
-REGISTER_ZYGISK_MODULE(SpoofModule)
+REGISTER_ZYGISK_MODULE(SpoofModule);
