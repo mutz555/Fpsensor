538a3c8046756e891fdc86fa677ee58e3c91983a
mutz555 <mrbosscoc22@gmail.com>
2025-05-09 03:53:38 +0700

    Update main.cpp

--- jni/main.cpp
+++ jni/main.cpp
@@ -1,55 +1,34 @@
-#include <jni.h>
-#include <android/log.h>
-#include <android/native_window_jni.h>
-#include "zygisk.hpp"
-#include "xhook.h"
+#include <jni.h> #include <unistd.h> #include <android/log.h> #include <dlfcn.h> #include <cstring> #include <cstdlib> #include <sys/system_properties.h> #include "xhook.h" #include "zygisk.hpp"
 
-#define LOG_TAG "ZygiskXHook"
-#define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
+#define LOG_TAG "ZygiskSpoof" #define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, VA_ARGS)
 
-// Hook target functions
-static void (*orig_ANativeWindow_disconnect)(ANativeWindow *window) = nullptr;
-static void my_ANativeWindow_disconnect(ANativeWindow *window) {
-    LOGI("Hooked: ANativeWindow_disconnect");
-    if (orig_ANativeWindow_disconnect) orig_ANativeWindow_disconnect(window);
+const char *target_apps[] = { "com.mobile.legends", "com.miHoYo.GenshinImpact", "com.miHoYo.hkrpg", "com.tencent.ig", "com.activision.callofduty.shooter" };
+
+bool shouldSpoof(const char *packageName) { for (const char *target : target_apps) { if (strcmp(packageName, target) == 0) return true; } return false; }
+
+void spoof_props() { struct prop_pair { const char *key; const char *val; } props[] = { {"ro.product.brand", "samsung"}, {"ro.product.manufacturer", "samsung"}, {"ro.product.device", "dm3q"}, {"ro.product.model", "SM-S928B"}, {"ro.product.name", "dm3qxx"}, {"ro.build.product", "dm3q"}, {"ro.build.fingerprint", "samsung/dm3qxx/dm3q:14/UP1A.231005.007/S928BXXU1AXB5:user/release-keys"}, {"ro.build.version.release", "14"}, {"ro.build.version.sdk", "34"}, {"ro.board.platform", "kalama"}, {"ro.hardware.soc.manufacturer", "Qualcomm Technologies, Inc."}, {"ro.hardware.soc.model", "SM8650"} };
+
+for (auto &p : props) {
+    __system_property_set(p.key, p.val);
+    LOGI("Spoofed %s = %s", p.key, p.val);
 }
 
-static int (*orig_ANativeWindow_setBuffersGeometry)(ANativeWindow* window, int w, int h, int format) = nullptr;
-static int my_ANativeWindow_setBuffersGeometry(ANativeWindow* window, int w, int h, int format) {
-    LOGI("Hooked: ANativeWindow_setBuffersGeometry w=%d h=%d format=%d", w, h, format);
-    return orig_ANativeWindow_setBuffersGeometry 
-        ? orig_ANativeWindow_setBuffersGeometry(window, w, h, format)
-        : -1;
 }
 
-// JNI_OnLoad
-extern "C"
-jint JNI_OnLoad(JavaVM *vm, void *reserved) {
-    LOGI("JNI_OnLoad called");
-    return JNI_VERSION_1_6;
+class SpoofModule : public zygisk::ModuleBase { public: void onLoad(zygisk::Api *api, JNIEnv *env) override { LOGI("[ZygiskSpoof] Module loaded"); }
+
+void preAppSpecialize(zygisk::AppSpecializeArgs *args) override {
+    const char *pkg = args->nice_name ? args->env->GetStringUTFChars(args->nice_name, nullptr) : "";
+    if (shouldSpoof(pkg)) {
+        LOGI("[ZygiskSpoof] Target app detected: %s", pkg);
+        spoof_props();
+    } else {
+        LOGI("[ZygiskSpoof] Non-target app: %s", pkg);
+    }
+    if (pkg && args->nice_name) args->env->ReleaseStringUTFChars(args->nice_name, pkg);
 }
 
-// Zygisk Module
-class MyZygiskModule : public zygisk::ModuleBase {
-public:
-    void onLoad(zygisk::Api *api, JNIEnv *env) override {
-        LOGI("Zygisk onLoad start");
-
-        xhook_register(".*", "ANativeWindow_disconnect", (void *)my_ANativeWindow_disconnect, (void **)&orig_ANativeWindow_disconnect);
-        xhook_register(".*", "ANativeWindow_setBuffersGeometry", (void *)my_ANativeWindow_setBuffersGeometry, (void **)&orig_ANativeWindow_setBuffersGeometry);
-        xhook_refresh(0);
-
-        LOGI("xhook registered successfully");
-    }
-
-    // HAPUS 'const' DI BAWAH INI
-    void postAppSpecialize(zygisk::AppSpecializeArgs *args) override {
-        LOGI("preAppSpecialize");
-    }
-
-    void preAppSpecialize(zygisk::AppSpecializeArgs *args) override {
-        LOGI("postAppSpecialize");
-    }
 };
 
-REGISTER_ZYGISK_MODULE(MyZygiskModule)
+REGISTER_ZYGISK_MODULE(SpoofModule)
+
