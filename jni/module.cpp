21353bb1754682cac73e5da31a937a1fc2ba6cd5
mutz555 <mrbosscoc22@gmail.com>
2025-05-11 02:21:33 +0700

    Update module.cpp

--- jni/module.cpp
+++ jni/module.cpp
@@ -9,39 +9,47 @@ extern "C" {
     void apply_hooks_if_target_app(const char* process_name);
 }
 
+#define LOG_TAG "SnapdragonSpoof"
+#define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
+#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
+
 // Implementasi modul Zygisk
 class SnapdragonSpoofModule : public zygisk::ModuleBase {
 public:
     void onLoad(zygisk::Api *api, JNIEnv *env) override {
         this->api = api;
         this->env = env;
-        __android_log_print(ANDROID_LOG_INFO, "SnapdragonSpoof", "SnapdragonSpoof module loaded");
+        LOGI("SnapdragonSpoof module loaded (onLoad)");
         init_snapdragon_spoof();
     }
 
     void preAppSpecialize(zygisk::AppSpecializeArgs *args) override {
+        if (!env) {
+            LOGE("JNIEnv is NULL di preAppSpecialize!");
+            return;
+        }
         const char *process = nullptr;
         if (args->nice_name) {
             process = env->GetStringUTFChars(args->nice_name, nullptr);
-            __android_log_print(ANDROID_LOG_INFO, "SnapdragonSpoof", "Checking process: %s", process);
+            LOGI("preAppSpecialize: nice_name = [%s]", process ? process : "NULL");
             apply_hooks_if_target_app(process);
             env->ReleaseStringUTFChars(args->nice_name, process);
         } else {
-            __android_log_print(ANDROID_LOG_INFO, "SnapdragonSpoof", "No process name provided, skipping");
+            LOGI("preAppSpecialize: nice_name NULL");
         }
     }
 
     void postAppSpecialize(const zygisk::AppSpecializeArgs *args) override {
-        __android_log_print(ANDROID_LOG_INFO, "SnapdragonSpoof", "Post app specialization");
+        LOGI("postAppSpecialize dipanggil");
     }
 
     void preServerSpecialize(zygisk::ServerSpecializeArgs *args) override {
-        api->setOption(0);
+        if (api) api->setOption(0);
     }
 
 private:
-    zygisk::Api *api;
-    JNIEnv *env;
+    zygisk::Api *api = nullptr;
+    JNIEnv *env = nullptr;
 };
 
 // ---- ENTRY POINT ZYGISK MODERN ----
