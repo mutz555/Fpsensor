f62a3c91ce2954b0406c26cd0ffb1d386a86275a
mutz555 <mrbosscoc22@gmail.com>
2025-05-11 04:23:49 +0700

    Update module.cpp

--- jni/module.cpp
+++ jni/module.cpp
@@ -6,7 +6,7 @@
 // Deklarasi fungsi dari main.cpp yang akan dipanggil
 extern "C" {
     void init_snapdragon_spoof();
-    void apply_hooks_if_target_app(const char* package_name);
+    void apply_hooks_if_target_app(const char* process_name);
 }
 
 #define LOG_TAG "SnapdragonSpoof"
@@ -28,16 +28,16 @@ public:
             LOGE("JNIEnv is NULL di preAppSpecialize!");
             return;
         }
-        const char *pkg_name = nullptr;
-if (args->package_name) {
-    pkg_name = env->GetStringUTFChars(args->package_name, nullptr);
-    LOGI("preAppSpecialize: package_name = [%s]", pkg_name ? pkg_name : "NULL");
-    apply_hooks_if_target_app(pkg_name);
-    env->ReleaseStringUTFChars(args->package_name, pkg_name);
-} else {
-            LOGI("preAppSpecialize: package_name NULL");
-        }
-    }
+        const char *process = nullptr;  
+    if (args->nice_name) {  
+        process = env->GetStringUTFChars(args->nice_name, nullptr);  
+        LOGI("preAppSpecialize: nice_name = [%s]", process ? process : "NULL");  
+        apply_hooks_if_target_app(process);  
+        env->ReleaseStringUTFChars(args->nice_name, process);  
+    } else {  
+        LOGI("preAppSpecialize: nice_name NULL");  
+    }  
+}
 
     void postAppSpecialize(const zygisk::AppSpecializeArgs *args) override {
         LOGI("postAppSpecialize dipanggil");
