3b00784de666f2c4baaee40fec284ae5556d3055
mutz555 <mrbosscoc22@gmail.com>
2025-05-10 23:43:04 +0700

    Update module.cpp

--- jni/module.cpp
+++ jni/module.cpp
@@ -3,54 +3,39 @@
 #include <android/log.h>
 #include "zygisk.hpp"
 
-// Pastikan ini adalah modul utama Zygisk dengan entry point yang benar
-#define ZYGISK_MODULE_NAME "SnapdragonSpoof"
-#define ZYGISK_LOG_TAG "SnapdragonSpoof"
-#define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG, ZYGISK_LOG_TAG, __VA_ARGS__)
-#define LOGI(...) __android_log_print(ANDROID_LOG_INFO, ZYGISK_LOG_TAG, __VA_ARGS__)
-#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, ZYGISK_LOG_TAG, __VA_ARGS__)
-
 // Deklarasi fungsi dari main.cpp yang akan dipanggil
 extern "C" {
     void init_snapdragon_spoof();
     void apply_hooks_if_target_app(const char* process_name);
 }
 
-// Implementasi modul Zygisk dengan entry point yang benar
+// Implementasi modul Zygisk
 class SnapdragonSpoofModule : public zygisk::ModuleBase {
 public:
     void onLoad(zygisk::Api *api, JNIEnv *env) override {
         this->api = api;
         this->env = env;
-        LOGI("SnapdragonSpoof module loaded");
-        
-        // Inisialisasi modul
+        __android_log_print(ANDROID_LOG_INFO, "SnapdragonSpoof", "SnapdragonSpoof module loaded");
         init_snapdragon_spoof();
     }
 
     void preAppSpecialize(zygisk::AppSpecializeArgs *args) override {
         const char *process = nullptr;
-        
         if (args->nice_name) {
             process = env->GetStringUTFChars(args->nice_name, nullptr);
-            LOGI("Checking process: %s", process);
-            
-            // Panggil fungsi untuk mengecek dan menerapkan hook jika diperlukan
+            __android_log_print(ANDROID_LOG_INFO, "SnapdragonSpoof", "Checking process: %s", process);
             apply_hooks_if_target_app(process);
-            
             env->ReleaseStringUTFChars(args->nice_name, process);
         } else {
-            LOGI("No process name provided, skipping");
+            __android_log_print(ANDROID_LOG_INFO, "SnapdragonSpoof", "No process name provided, skipping");
         }
     }
 
     void postAppSpecialize(const zygisk::AppSpecializeArgs *args) override {
-        // Kode yang akan dijalankan setelah app dispecialize
-        LOGI("Post app specialization");
+        __android_log_print(ANDROID_LOG_INFO, "SnapdragonSpoof", "Post app specialization");
     }
 
     void preServerSpecialize(zygisk::ServerSpecializeArgs *args) override {
-        // Tidak perlu dijalankan di system_server
         api->setOption(0);
     }
 
@@ -59,5 +44,10 @@ private:
     JNIEnv *env;
 };
 
-// Register modul Zygisk - ini adalah entry point utama
-REGISTER_ZYGISK_MODULE(SnapdragonSpoofModule)
+// ---- ENTRY POINT ZYGISK WAJIB ----
+static SnapdragonSpoofModule g_module;
+
+// Untuk Zygisk modern (2023-2025): pointer to pointer
+extern "C" void registerModule(zygisk::ModuleBase **module) {
+    *module = &g_module;
+}
