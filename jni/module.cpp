43c8fe1e5bba64768034ab1826956a50f7180749
mutz555 <mrbosscoc22@gmail.com>
2025-05-11 07:36:51 +0700

    Update module.cpp

--- jni/module.cpp
+++ jni/module.cpp
@@ -1,6 +1,5 @@
 #include <unistd.h>
 #include <cstdlib>
-#include <cstring>
 #include <android/log.h>
 #include "zygisk.hpp"
 
@@ -14,86 +13,44 @@ extern "C" {
 #define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
 #define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
 
-// Helper function to extract package name from app_data_dir
-static const char* extract_package_name(const char* app_data_dir) {
-    if (!app_data_dir) return nullptr;
-
-    const char *prefix1 = "/data/user/0/";
-    const char *prefix2 = "/data/data/";
-
-    if (strstr(app_data_dir, prefix1) == app_data_dir) {
-        return app_data_dir + strlen(prefix1);
-    } else if (strstr(app_data_dir, prefix2) == app_data_dir) {
-        return app_data_dir + strlen(prefix2);
-    }
-
-    return nullptr;
-}
-
-// Variabel global untuk akses API/JNI
-zygisk::Api* g_api = nullptr;
-JNIEnv* g_env = nullptr;
-
-// Kelas utama modul Zygisk
+// Implementasi modul Zygisk
 class SnapdragonSpoofModule : public zygisk::ModuleBase {
 public:
-    void onLoad(zygisk::Api* api, JNIEnv* env) override {
+    void onLoad(zygisk::Api *api, JNIEnv *env) override {
+        this->api = api;
+        this->env = env;
         LOGI("SnapdragonSpoof module loaded (onLoad)");
         init_snapdragon_spoof();
-        g_api = api;
-        g_env = env;
     }
 
-    void preAppSpecialize(zygisk::AppSpecializeArgs* args) override {
-    JNIEnv* env = nullptr;
-    if (g_api) g_api->get_jni_env(&env);
-    if (!env) {
-        LOGE("JNIEnv is NULL di preAppSpecialize!");
-        return;
-    }
-
-    const char *nice_name = nullptr;
-    if (args->nice_name) {
-        nice_name = env->GetStringUTFChars(args->nice_name, nullptr);
-        LOGI("preAppSpecialize: nice_name = [%s]", nice_name ? nice_name : "NULL");
-    }
-
-    const char *app_data_dir = nullptr;
-    const char *package_name = nullptr;
-    if (args->app_data_dir) {
-        app_data_dir = env->GetStringUTFChars(args->app_data_dir, nullptr);
-        LOGI("preAppSpecialize: app_data_dir = [%s]", app_data_dir ? app_data_dir : "NULL");
-
-        package_name = extract_package_name(app_data_dir);
-        if (package_name) {
-            LOGI("Extracted package name: [%s]", package_name);
-            apply_hooks_if_target_app(package_name);
+    void preAppSpecialize(zygisk::AppSpecializeArgs *args) override {
+        if (!env) {
+            LOGE("JNIEnv is NULL di preAppSpecialize!");
+            return;
         }
-    }
-
-    if (!package_name && nice_name) {
-        LOGI("Falling back to nice_name as package identifier");
-        apply_hooks_if_target_app(nice_name);
-    }
-
-    if (nice_name) {
-        env->ReleaseStringUTFChars(args->nice_name, nice_name);
-    }
-    if (app_data_dir) {
-        env->ReleaseStringUTFChars(args->app_data_dir, app_data_dir);
-    }
+        const char *process = nullptr;  
+    if (args->nice_name) {  
+        process = env->GetStringUTFChars(args->nice_name, nullptr);  
+        LOGI("preAppSpecialize: nice_name = [%s]", process ? process : "NULL");  
+        apply_hooks_if_target_app(process);  
+        env->ReleaseStringUTFChars(args->nice_name, process);  
+    } else {  
+        LOGI("preAppSpecialize: nice_name NULL");  
+    }  
 }
 
-    void postAppSpecialize(const zygisk::AppSpecializeArgs* /*args*/) override {
+    void postAppSpecialize(const zygisk::AppSpecializeArgs *args) override {
         LOGI("postAppSpecialize dipanggil");
     }
 
-    void preServerSpecialize(zygisk::ServerSpecializeArgs* /*args*/) override {
-        if (g_api) g_api->setOption(0);
+    void preServerSpecialize(zygisk::ServerSpecializeArgs *args) override {
+        if (api) api->setOption(0);
     }
+
 private:
-    // Opsional: kamu bisa simpan pointer di sini jika tidak ingin pakai variabel global
+    zygisk::Api *api = nullptr;
+    JNIEnv *env = nullptr;
 };
 
-// ENTRY POINT ZYGISK
+// ---- ENTRY POINT ZYGISK MODERN ----
 REGISTER_ZYGISK_MODULE(SnapdragonSpoofModule)
